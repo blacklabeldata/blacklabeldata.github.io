7f8d5aa57a959862f4ed2a73bd58b9189bf25836
Jarvis <jarvis.martin@yahoo.com>
2015-09-20 20:36:03 -0500

    added typing sound effects and transition from the intro to a static header

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ riot.tag('footer', '<div class="footer"> <span class="info"> info@blacklabeldata
 
 });
 
-riot.tag('logo', '<div class="logo" name="logo"> <span id="logoTxt" class="logoTxt" name="logoTxt"></span> <span name="nextTxt" class="nextTxt"></span> </div>', 'logo , [riot-tag="logo"] { width: 100%; text-align: center; } logo .logo , [riot-tag="logo"] .logo { font-family: "impactLabel"; font-size: 5rem; margin-left: auto; margin-right: auto; } logo .logoTxt , [riot-tag="logo"] .logoTxt { background-color: #fff; } logo .nextTxt , [riot-tag="logo"] .nextTxt { background-color: #fff; } }', function(opts) {
+riot.tag('header', '<div id="headWrap"> <div class="logo" name="logo"> <span id="logoTxt" class="logoTxt" name="logoTxt">Black Label Data</span> </div> <div class="email" name="email"> info@blacklabeldata.io </div> </div>', 'header , [riot-tag="header"] { width: 100%; position: fixed; left: 0; top: 0; z-index: 999; } header .logo , [riot-tag="header"] .logo { font-family: "impactLabel"; font-size: 5rem; text-align: left; } header .logoTxt , [riot-tag="header"] .logoTxt { background-color: #fff; margin-left: 30px; } header .email , [riot-tag="header"] .email { font-size: 1.1rem; font-family: monospace; color: #fff; letter-spacing: 3px; margin-top: -34px; margin-right: 30px; float: right; } header #headWrap , [riot-tag="header"] #headWrap { opacity: 0; padding-top: 15px; } }', function(opts) {
   var self = this;
 
 
@@ -11,85 +11,43 @@ riot.tag('logo', '<div class="logo" name="logo"> <span id="logoTxt" class="logoT
 
   var logoTxt = this.logoTxt;
   var nextTxt = this.nextTxt;
+  var email = this.email;
+  var mt = '0';
 
+  var w = window.innerWidth;
+  var h = window.innerHeight;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  var fadeIn = function() {
+  var fadeIn = function(tgt) {
     var j = 0;
 
     var fi = setInterval( function(){
-      var info = document.getElementsByClassName('info');
 
       if(j >= 1) {
         clearInterval(fi);
       }
-      info[0].style.opacity = j;
+      tgt.style.opacity = j;
       j = j+0.01;
 
-    }, 50);
+    }, 10);
   }
 
   this.on('mount', function() {
-
-    var str = "Black Label Data";
-    var i = 0;
-
-    var inc = setInterval( function() {
-
-
-
-      logoTxt.innerHTML += str[i];
-      i++;
-      if(i === str.length) {
-        clearInterval(inc);
-        fadeIn();
-      }
-    }, 160);
-
-
-
-
-
-
-
-
 
   });
 
   this.on('update', function() {
 
-    var w = window.innerWidth;
 
 
 
 
-    var h = window.innerHeight;
 
 
-    if(w >= 610) { // Bigger Screen
-      var mt = h/3;
-      logo.style.marginTop = mt + 'px';
-    } else { // Portrait Mode
-      var mt = h/5.5;
-      logo.style.marginTop = mt + 'px';
-    }
 
-    if(w < 811 && h < 456) {
-      var mt = h/8;
-      logo.style.marginTop = mt + 'px';
-    }
+
+
+
+
 
   });
 
@@ -100,49 +58,97 @@ riot.tag('logo', '<div class="logo" name="logo"> <span id="logoTxt" class="logoT
   
 });
 
-riot.tag('navbar', '<div class="row"> <div class="nav-bar col-sm-8 col-sm-offset-2"> <a each="{ sections }" href="#{ title }" class="col-xs-3" > <button class="col-xs-12 { active: active }" type="button"> { title } </button> </a> </div> </div>', 'navbar .nav-bar , [riot-tag="navbar"] .nav-bar { margin-top: 20px; margin-bottom: 20px; height: 64px; z-index: 999; display: -ms-flexbox; display: flex; -ms-flex-align: center; align-items: center; -ms-flex-pack: justify; justify-content: center; padding: 0; } navbar .nav-bar a , [riot-tag="navbar"] .nav-bar a { padding: 0; } navbar .nav-bar button , [riot-tag="navbar"] .nav-bar button { height: 60px; background-color: #00ADD8; color: #FFF; border: none; border-right: 1px solid #0185A6; border-left: 1px solid #bbb; } navbar .nav-bar a:first-of-type button , [riot-tag="navbar"] .nav-bar a:first-of-type button { border-left: none; } navbar .nav-bar button:hover , [riot-tag="navbar"] .nav-bar button:hover { background-color: #0185A6; } navbar .nav-bar button.active , [riot-tag="navbar"] .nav-bar button.active { background-color: #0185A6; }', function(opts) {
-    this.sections = [
-      { title: 'Support', active: false },
-      { title: 'RMA', active: false },
-      { title: 'FAQ', active: false },
-      { title: 'Learn', active: false }
-    ];
+riot.tag('intro', '<div class="intro" name="intro"> <span id="introTxt" class="introTxt" name="introTxt"></span> <span name="nextTxt" class="nextTxt"></span> </div> <div class="email" name="email"> info@blacklabeldata.io </div> <audio autoplay> <source src="./assets/av/typewriter.ogg" type="audio/ogg"> Your browser does not support the audio element. </audio>', 'intro , [riot-tag="intro"] { } intro .intro , [riot-tag="intro"] .intro { font-family: "impactLabel"; font-size: 5rem; width: 100%; text-align: center; } intro .introTxt , [riot-tag="intro"] .introTxt { background-color: #fff; } intro .nextTxt , [riot-tag="intro"] .nextTxt { background-color: #fff; } intro .email , [riot-tag="intro"] .email { font-size: 1.1rem; font-family: monospace; color: #fff; letter-spacing: 3px; opacity: 0; margin-top: -36px; margin-right: 30px; float: right; } }', function(opts) {
+  var self = this;
 
+  var intro = this.intro;
+  var introTxt = this.introTxt;
+  var nextTxt = this.nextTxt;
+  var email = this.email;
+  var mt = '0';
 
+  var w = window.innerWidth;
+  var h = window.innerHeight;
 
-    self = this;
+  var fadeIn = function(tgt) {
+    var j = 0;
 
-    function HandleRoute(hash) {
+    var fi = setInterval( function(){
 
-      var match = false;
-
-      self.sections.forEach( function(element) {
-        if(element.title == hash) {
-          element.active = true;
-          match = true;
-        } else {
-          element.active = false;
-        }
-      });
-
-      if(match == false) {
-        riot.route('#Home');
+      if(j >= 1) {
+        clearInterval(fi);
       }
+      tgt.style.opacity = j;
+      j = j+0.01;
 
-      self.update();
+    }, 10);
+  }
+
+  this.on('mount', function() {
+
+    var str = "Black Label Data";
+    var i = 0;
+
+    var inc = setInterval( function() {
+      introTxt.innerHTML += str[i];
+      i++;
+      if(i === str.length) {
+        clearInterval(inc);
+      }
+    }, 160);
+
+    var rearrange = setTimeout( function() {
+
+      var translate = setInterval( function() {
+        if(mt <= 60){
+          clearInterval(translate);
+          intro.style.marginTop = '60px';
+
+          if(w < 1000) { // smaller Screen
+            introTxt.innerHTML = 'BLD';
+            email.style.marginTop = '5px';
+          }
+        }
+
+        mt -= 15;
+        intro.style.marginTop = mt + 'px';
+
+      }, 20);
+
+  }, 4300);
+
+
+    var makeRoom4Header = setTimeout( function() {
+
+      intro.style.display = 'none';
+
+      var hw = document.getElementById('headWrap');
+      fadeIn(hw);
+
+    }, 5000);
+
+  });
+
+  this.on('update', function() {
+
+    if(w >= 610) { // Bigger Screen
+      mt = h/3;
+      intro.style.marginTop = mt + 'px';
+    } else { // Portrait Mode
+      mt = h/5.5;
+      intro.style.marginTop = mt + 'px';
     }
 
-    riot.route.exec(function(hash) {
+    if(w < 811 && h < 456) {
+      mt = h/8;
+      intro.style.marginTop = mt + 'px';
+    }
 
-      HandleRoute(hash);
+  });
 
-    });
-
-    riot.route(function(hash) {
-
-      HandleRoute(hash);
-
-    });
+  window.onresize = function(event) {
+    self.update();
+  };
 
   
 });
